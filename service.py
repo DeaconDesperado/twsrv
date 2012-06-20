6fb78d47b2aa83d078491526bfbe281ef5d03c26
Mark Grey <mark@mrak.(none)>
2012-06-20 13:46:24 -0400

    server working with vhosts

--- service.py
+++ service.py
@@ -2,8 +2,17 @@ from twisted.web.server import Site
 from twisted.web.wsgi import WSGIResource
 from twisted.web import static,server,vhost,script
 from twisted.internet import reactor
+from twisted.python import log
 import sys,os
+import json
+import logging
 
+observer = log.PythonLoggingObserver()
+observer.start()
+
+core_log = logging.getLogger('twsrv')
+sh = log.handlers.StreamHandler()
+core_log.addHandler(sh)
 
 root = vhost.NameVirtualHost()
 
@@ -16,7 +25,7 @@ for host in hosts:
     path,module,app = hosts[host]
     sys.path.append(path)
     exec("from %s import %s" % (module,app))
-
+    log.msg('Setting up host %s', server_name)
     root.addHost(server_name,WSGIResource(reactor,reactor.getThreadPool(),app))
 
 reactor.listenTCP(80,Site(root))
